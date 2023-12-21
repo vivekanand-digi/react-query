afb7205e99b1f84c3b8c30a6e7c241ca52b22e12
vivekanand-digi <117620313+vivekanand-digi@users.noreply.github.com>
2023-12-21 19:04:37 +0530

    error fixed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,7 @@ import { getTodos, addTodo, updateTodo, deleteTodo } from "../api/TodosApi"
 
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 // import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons"
-import { useState } from 'react'
+import { useState } from 'react';
 
 const TodoList = () => {
     const [newTodo, setNewTodo] = useState('')
